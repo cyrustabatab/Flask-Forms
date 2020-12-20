39f77c2cc011715710a5e63ef81169b4f1c9443e
Cyrus Tabatabai-Yazdi <24766642+cyrustabatab@users.noreply.github.com>
2020-12-19 23:52:54 -0800

    Added Form validation and Flask bootstrap to make website look nicer

--- login_form.py
+++ login_form.py
@@ -1,13 +1,17 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField
-from wtforms.validators import DataRequired
+from wtforms import StringField,PasswordField,SubmitField
+from wtforms.validators import DataRequired,Email,Length
+from wtforms.fields.html5 import EmailField
 
 
 
 class LoginForm(FlaskForm):
 
-    email = StringField('email',validators=[DataRequired()])
-    password = StringField('password',validators=[DataRequired()])
+    email = StringField('Email',validators=[DataRequired(),Email()])
+    password = PasswordField('Password',validators=[DataRequired(),Length(min=8)])
+    submit = SubmitField("Log In")
+
+
 
 
 
--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request
+from flask_bootstrap import Bootstrap
 from login_form import LoginForm
 
 app = Flask(__name__)
+Bootstrap(app)
+
+
 
 app.secret_key = "hello_world"
 
@@ -9,9 +13,17 @@ app.secret_key = "hello_world"
 def home():
     return render_template('index.html')
 
-@app.route('/login')
+@app.route('/login',methods=['GET','POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        email = (form.email.data)
+        password =  (form.password.data)
+        if email == "admin@email.com" and password == "12345678":
+            return render_template("success.html")
+        else:
+            return render_template("denied.html")
+
     return render_template('login.html',form=form)
 
 if __name__ == '__main__':
